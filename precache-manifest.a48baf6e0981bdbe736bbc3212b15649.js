89b3a8fe64f8a27702015e5fe8589f11ed2190e9
patrickshirazi <pshirazi@iastate.edu>
2020-01-07 20:14:24 -0600

    Updates

---
+++ precache-manifest.a48baf6e0981bdbe736bbc3212b15649.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa346660627d1ba57f6e85441ef0bf1e",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "d6b3250afcfbd665e6e7",
+    "url": "/my-portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e1c59c8366ad32a6ac0e",
+    "url": "/my-portfolio/static/js/2.e892de34.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-portfolio/static/js/2.e892de34.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d6b3250afcfbd665e6e7",
+    "url": "/my-portfolio/static/js/main.a4693f41.chunk.js"
+  },
+  {
+    "revision": "833714ca7ecb977107f9",
+    "url": "/my-portfolio/static/js/runtime-main.a5eef32f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
